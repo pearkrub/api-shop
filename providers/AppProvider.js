f1df764f073ee66cbd050a42de5f9d6705217802
Praibool Namwong <praibool.piak@gmail.com>
2019-03-08 16:39:48 +0700

    map http message from code

--- providers/AppProvider.js
+++ providers/AppProvider.js
@@ -3,12 +3,20 @@ const { ServiceProvider } = require('@adonisjs/fold')
 class AppProvider extends ServiceProvider {
     async boot() {
         const Response = use('Adonis/Src/Response')
+        const responses =
+        {
+            200: 'OK',
+            400: 'badRequest',
+            401: 'unauthorized',
+            403: 'forbidden',
+            404: 'notFound'
+        }
 
         Response.macro('customResponse', (status, data) => {
             const reponse = {
                 status: {
                     code: status,
-                    status: 'ok'
+                    status: responses[status] ? responses[status] : 'OK'
                 },
                 data: data
             }
